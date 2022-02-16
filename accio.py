1b4ccd98e64209fd8d8e1109e6a6dfb96ae595c2
Anthony Russell <arussell@renewedvision.com>
2022-02-16 15:55:23 -0500

    Now works with python 3

--- accio.py
+++ accio.py
@@ -14,27 +14,27 @@ If you're still confused, see the example json files or reach out to me
 
 '''
 
-
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from http.server import BaseHTTPRequestHandler, HTTPServer
+#from http.server.HTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 import sys
 import json
 import time
 
 def printHelp():
-	print ""
-	print "Accio"
-	print "Author: Anthony Russell"
-	print "Twitter: @DotNetRussell"
-	print "Blog: https://DotNetRussell.com"
-	print "GitHub: https://github.com/DotNetRussell"
-	print ""
-	print "To Use:"
-	print "python accio.py [config file path] [target localhost port] [calling port (because of CORS)]"
-	print ""
-	print "Example"
-	print "python accio.py /home/user/Desktop/accioConfig.json 8080 4200"
-	print ""
+	print("")
+	print("Accio")
+	print("Author: Anthony Russell")
+	print("Twitter: @DotNetRussell")
+	print("Blog: https://DotNetRussell.com")
+	print("GitHub: https://github.com/DotNetRussell")
+	print("")
+	print("To Use:")
+	print("python accio.py [config file path] [target localhost port] [calling port (because of CORS)]")
+	print("")
+	print("Example")
+	print("python accio.py /home/user/Desktop/accioConfig.json 8080 4200")
+	print("")
 
 if(len(sys.argv) != 4):
 	printHelp()
@@ -47,38 +47,41 @@ callingPort = int(sys.argv[3])
 routeDictionary = {}
 
 with open(configFile) as json_file:
-    data = json.load(json_file)
-    for route in data['routes']:
-        url = route['url']
-	definition = route['definition']
+	data = json.load(json_file)
+	for route in data['routes']:
+		url = route['url']
+		definition = route['definition']
 
-	if('method' in definition.keys()) :
-		url = url + "_" + str(definition['method']).lower()
-	else :
-		url = url + "_get"
+		if('method' in definition.keys()) :
+			url = url + "_" + str(definition['method']).lower()
+		else :
+			url = url + "_get"
 
-	routeDictionary[url] = definition
+		routeDictionary[url] = definition
 
 def generateResponse(self, method):
-
+	print('Generating response')
 	if( (self.path + "_" + method) not in routeDictionary):
 		self.send_response(404)
-		print self.path + "_" + method + " - Not Found"
+		print(self.path + "_" + method + " - Not Found")
 		return
 
 	routeDefinition = routeDictionary[self.path + "_" + method]
 
 	if('delay' in routeDefinition.keys()) :
+		print('Running a delay')
 		time.sleep(routeDefinition['delay'])
 
 	jsonFilePath = routeDefinition["filePath"]
 	payloadData = open(jsonFilePath)
 	response = '\n'.join(payloadData.readlines())
+
+	print('sending response')
 	self.send_response(200)
 	self.send_header('Access-Control-Allow-Origin', 'http://localhost:' + str(callingPort));
 	self.send_header('Content-type','text/json')
 	self.end_headers()
-	self.wfile.write(response)
+	self.wfile.write(response.encode("utf-8"))
 
 	return
 
@@ -92,12 +95,12 @@ class requestHandler(BaseHTTPRequestHandler):
 
 try:
 	server = HTTPServer(('', targetPort), requestHandler)
-	print "Accio --% ~~~~ @X@ Server Active @X@"
-	print "Server Listening On Port " + str(targetPort)
-	print "Press CTRL+C to stop the server"
+	print("Accio --% ~~~~ @X@ Server Active @X@")
+	print("Server Listening On Port " + str(targetPort))
+	print("Press CTRL+C to stop the server")
 
 	server.serve_forever()
 
 except KeyboardInterrupt:
-	print 'Shutting down the web server'
+	print('Shutting down the web server')
 	server.socket.close()
